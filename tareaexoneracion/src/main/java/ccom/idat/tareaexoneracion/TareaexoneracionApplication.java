f154853138251f50015b0ce6a0e581d1217c863b
R-SIGMA <84481623+R-SIGMA@users.noreply.github.com>
2022-07-14 19:18:37 -0500

    primer commit

---
+++ tareaexoneracion/src/main/java/ccom/idat/tareaexoneracion/TareaexoneracionApplication.java
@@ -0,0 +1,13 @@
+package ccom.idat.tareaexoneracion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaexoneracionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaexoneracionApplication.class, args);
+	}
+
+}
---
+++ tareaexoneracion/src/test/java/ccom/idat/tareaexoneracion/TareaexoneracionApplicationTests.java
@@ -0,0 +1,13 @@
+package ccom.idat.tareaexoneracion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaexoneracionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
